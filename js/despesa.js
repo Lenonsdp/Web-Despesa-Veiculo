169a370fef27bad8a31d78172808cddb019d9e36
Lenon Szynweslki De Paula <lenonsdp@hotmail.com>
2019-11-24 18:29:38 -0200

    Filtro por data

--- js/despesa.js
+++ js/despesa.js
@@ -4,8 +4,7 @@ $(function() {
 
 	vincularEventos();
 
-	var idDespesa = obterIdUrl();
-	console.log(idDespesa);
+	var idDespesa = obterIdUrl();	
 	if (idDespesa != false) {
 		editarDespesa(idDespesa);
 		$('#salvar').val('Editar');
@@ -51,6 +50,18 @@ function vincularEventos() {
 		});
 	});
 
+	$('#dataIni').on('change', function() {		
+		if ($('#dataIni').val() != '' && $('#dataFim').val() != '' && $('#veiculo_despesa').val() ) {
+			listar();
+		}
+	});
+
+	$('#dataFim').on('change', function() {
+		if ($('#dataIni').val() != '' && $('#dataFim').val() != '' && $('#veiculo_despesa').val() ) {
+			listar();
+		}
+	});
+
 	$('#incluir_despesa').on('click', function() {
 		$('#box_incluirDespesa').each(function() {
 			this.reset();
@@ -71,13 +82,12 @@ function vincularEventos() {
 		var camposObrigatorios = ['#valor', '#data'];
 		var idDespesa = obterIdUrl();
 		$(camposObrigatorios.join(', ')).parent().removeClass('warning');
-
 		if ($('#valor').val().trim() != '' && $('#data').val() != '') {
 			var valor = $("#valor").val();
 			valor = valor.toString().replace('R$', '');
 			var json = {
 				"valor": parseFloat(valor),
-				"data": $("#data").val(),
+				"data": formatDate($("#data").val()),
 				"observacoes": $("#observacoes").val(),
 				"tipo": $("#tipo").val(),
 				"idCarro": parseInt($('#veiculo_despesa').val())
@@ -190,21 +200,39 @@ function vincularEventos() {
 }
 
 function listar() {
-	$.ajax({
-		type: 'GET',
-		headers:{    
-			'Accept': 'application/json',
-			'Content-Type': 'application/json',
-			'Access-Control-Allow-Origin': '*' 
-		},
-		url: 'https://apirestdev.herokuapp.com/api/despesas/' + $('#veiculo_despesa').val(),		
-		dataType: 'json',		
-		'success': function(resp) {
-			console.log(resp);
-			// ultimaPagina = Math.ceil(dados['totalRegistros'] / dados['paginacao']) - 1;
-			mostrarDadosTabela(resp);
-		}
-	});
+	var dataIni = formatDate($('#dataIni').val());
+	var dataFim = formatDate($('#dataFim').val()); 		
+	if (dataIni != '' && dataIni != '') {
+		$.ajax({
+			type: 'GET',
+			headers:{    
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*' 
+			},
+			url: 'https://apirestdev.herokuapp.com/api/despesas/' + $('#veiculo_despesa').val() + '/' + dataIni  + '/' + dataFim,		
+			dataType: 'json',		
+			'success': function(resp) {				
+				// ultimaPagina = Math.ceil(dados['totalRegistros'] / dados['paginacao']) - 1;
+				mostrarDadosTabela(resp);
+			}
+		});
+	} else {
+		$.ajax({
+			type: 'GET',
+			headers:{    
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin': '*' 
+			},
+			url: 'https://apirestdev.herokuapp.com/api/despesas/' + $('#veiculo_despesa').val(),		
+			dataType: 'json',		
+			'success': function(resp) {				
+				// ultimaPagina = Math.ceil(dados['totalRegistros'] / dados['paginacao']) - 1;
+				mostrarDadosTabela(resp);
+			}
+		});
+	}
 }
 
 function editarDespesa(id) {
@@ -216,22 +244,28 @@ function mostrarDadosTabela(despesas) {
 	$('#tabela_despesa tbody').empty();
 	var totalDespesa = 0;
 
-	$.each(despesas, function(key, despesas) {
-		totalDespesa += despesas.valor;
+	$.each(despesas, function(key, despesas) { 	
+		totalDespesa += despesas.valor;				
 		$('#tabela_despesa tbody').append(
 			$('<tr>', {'data-id': despesas.id}).append(
 				$('<td>').append(
 					$('<input>', {'type': 'checkbox'})
 				),
-				$('<td>', {'text': despesas.data}),
+				$('<td>', {'text': formatDate(despesas.data, 'pt-br')}),
 				$('<td>', {'text': despesas.tipo}),
 				$('<td>', {'text': despesas.valor.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"})})
 			)
-		);                        
+		)                      
 	});
 
-	$('#valorTotal').val(totalDespesa.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"}));
-
+	$('#tabela_despesa tbody').append(
+		$('<tr>').append(
+			$('<th>', {'text': 'Total'}),
+			$('<th>'),
+			$('<th>'),
+			$('<th>', {'text': totalDespesa.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"})})
+		)
+	)
 };
 
 function obterIdUrl() {
@@ -258,8 +292,16 @@ function obterDadosDespesa(idDespesa) {
 		success: function(despesa) {
 			$('#tipo').val(despesa.tipo);
 			$('#observacoes').val(despesa.observacoes);
-			$('#data').val(despesa.data);
+			$('#data').val(formatDate(despesa.data, 'pt-br'));
 			$('#valor').val(despesa.valor.toLocaleString("pt-BR", { style: "currency" , currency:"BRL"}));
 		}
 	});
+}
+
+function formatDate(data, formato) {
+  if (formato == 'pt-br') {
+	return (data.substr(0, 10).split('-').reverse().join('/'));
+  } else {
+	return (data.substr(0, 10).split('/').reverse().join('-'));
+  }
 }
